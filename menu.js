d9803716f3a43c3a3abdaee6400696356c780d27
ItsGyura <sharelockcod5@gmail.com>
2026-04-28 11:38:52 +0200

    Alapok létrehozása
    Minden oldal vázának elkészítése a crude alkalmazás elkészítése hozzá a megfelelő crude.php és connect.php
    login és registry php-k elkészítése

---
+++ menu.js
@@ -0,0 +1,20 @@
+const fejlec=`<header>
+<h1>Web Gyakorlati Beadandó</h1>
+<nav>
+<a href="index.html">Főoldal</a>
+<a href="kepek.html">Képek</a>
+<a href="kapcsolat.html">Kapcsolat</a>
+<a href="Crud.html">Crud</a>
+<a href="bejelentkezes.html">Bejelentkezés</a>
+</nav>
+</header>`;
+
+const lab=`<footer>
+Gyurászik György Marcell - ZX4R0A | Patkós Máté - CS9R44
+</footer>`;
+
+document.addEventListener("DOMContentLoaded",function(){
+document.body.insertAdjacentHTML("afterbegin",fejlec);
+document.body.insertAdjacentHTML("beforeend",lab);
+
+})
